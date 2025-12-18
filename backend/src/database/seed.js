1d353e759dd4b8f02b7439ca8ff59ebdaf916bec
Edu Moreno <e.m.morenolp@gmail.com>
2025-12-17 22:02:51 -0300

    feat: Update seed sources with new websites and selectors for improved data scraping

--- backend/src/database/seed.js
+++ backend/src/database/seed.js
@@ -6,53 +6,10 @@ const { db } = require('./init');
 function seedSources() {
     const sources = [
         {
-            name: 'Dev.to - JavaScript',
-            url: 'https://dev.to/t/javascript',
+            name: 'Xataka',
+            url: 'https://www.xataka.com',
             selectors: {
-                articleList: '.crayons-story',
-                title: '.crayons-story__title a',
-                image: '.crayons-story__cover img',
-                author: '.crayons-story__meta a.crayons-story__secondary',
-                content: '.crayons-article__main'
-            }
-        },
-        {
-            name: 'Dev.to - React',
-            url: 'https://dev.to/t/react',
-            selectors: {
-                articleList: '.crayons-story',
-                title: '.crayons-story__title a',
-                image: '.crayons-story__cover img',
-                author: '.crayons-story__meta a.crayons-story__secondary',
-                content: '.crayons-article__main'
-            }
-        },
-        {
-            name: 'Dev.to - Node',
-            url: 'https://dev.to/t/node',
-            selectors: {
-                articleList: '.crayons-story',
-                title: '.crayons-story__title a',
-                image: '.crayons-story__cover img',
-                author: '.crayons-story__meta a.crayons-story__secondary',
-                content: '.crayons-article__main'
-            }
-        },
-        {
-            name: 'Hacker News',
-            url: 'https://news.ycombinator.com',
-            selectors: {
-                articleList: '.athing',
-                title: '.titleline a',
-                author: '.hnuser',
-                content: '.comment'
-            }
-        },
-        {
-            name: 'CSS-Tricks',
-            url: 'https://css-tricks.com',
-            selectors: {
-                articleList: 'article.article-card',
+                articleList: 'article.recent-abstract',
                 title: 'h2 a',
                 image: 'img',
                 author: '.author-name',
@@ -60,14 +17,80 @@ function seedSources() {
             }
         },
         {
-            name: 'Smashing Magazine',
-            url: 'https://www.smashingmagazine.com/articles/',
+            name: 'Genbeta',
+            url: 'https://www.genbeta.com',
             selectors: {
-                articleList: 'article.article--post',
+                articleList: 'article.recent-abstract',
                 title: 'h2 a',
                 image: 'img',
-                author: '.author__name',
-                content: '.c-garfield-the-cat'
+                author: '.author-name',
+                content: '.article-content'
+            }
+        },
+        {
+            name: 'Hipertextual',
+            url: 'https://hipertextual.com/tecnologia',
+            selectors: {
+                articleList: 'article.post',
+                title: 'h2 a',
+                image: 'img',
+                author: '.author',
+                content: '.entry-content'
+            }
+        },
+        {
+            name: 'WWWhats New',
+            url: 'https://wwwhatsnew.com/category/tecnologia/',
+            selectors: {
+                articleList: 'article.post',
+                title: 'h2 a',
+                image: 'img',
+                author: '.author-name',
+                content: '.entry-content'
+            }
+        },
+        {
+            name: 'El Androide Libre',
+            url: 'https://elandroidelibre.elespanol.com',
+            selectors: {
+                articleList: 'article.article-item',
+                title: 'h2 a',
+                image: 'img',
+                author: '.author',
+                content: '.article-body'
+            }
+        },
+        {
+            name: 'MuyComputer',
+            url: 'https://www.muycomputer.com',
+            selectors: {
+                articleList: 'article.post',
+                title: 'h2 a',
+                image: 'img',
+                author: '.author-name',
+                content: '.entry-content'
+            }
+        },
+        {
+            name: 'Applesfera',
+            url: 'https://www.applesfera.com',
+            selectors: {
+                articleList: 'article.recent-abstract',
+                title: 'h2 a',
+                image: 'img',
+                author: '.author-name',
+                content: '.article-content'
+            }
+        },
+        {
+            name: 'FayerWayer',
+            url: 'https://www.fayerwayer.com',
+            selectors: {
+                articleList: 'article.article-card',
+                title: 'h2 a',
+                image: 'img',
+                author: '.author',
+                content: '.article-content'
             }
         }
     ];
